164c726ccd113ea322b10a306d2f2e343795055f
Satyam <satyampandit021@gmail.com>
2023-08-17 16:39:12 +0530

    second update

--- start.js
+++ start.js
@@ -5,12 +5,20 @@ require('dotenv').config()
 const app = express()
 
 const PORT = process.env.PORT || 8000
-// app.use(cors())
 
-app.get("/table", (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.send("Hey Dear!")
+app.use(cors())
+app.use(express.json())
+
+app.post("/table", (req, res) => {
+    const { text } = req.body
+    res.send("Data gone!")
+    console.log("comming data is:", text)
+})
+app.get("/", (req, res) => {
+
+
+    console.log(": data :")
+    res.send("Data came!")
 })
 
 app.listen(PORT, () => {
